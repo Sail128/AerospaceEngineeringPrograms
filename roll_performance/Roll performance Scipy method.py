52a54c52160f1e7243cc9e6fc96132593d927d86
r.voort <r.voort@me.com>
2018-10-02 17:04:52 +0200

    roll performance annotations

--- roll_performance/Roll performance Scipy method.py
+++ roll_performance/Roll performance Scipy method.py
@@ -4,19 +4,19 @@ from math import pi
 
 #geometry values
 lam = 0.29 #tip to root ratio (lambda)
-Cr = 6.76
+Cr = 6.76 #root chord length [m]
 
 #Set values
-b = 41.1     #m
+b = 41.1     # wing span [m]
 V_cruise = 233.105    #m/s
-P_req = ((30*pi)/180)/1.5 #rad/s
+P_req = ((30*pi)/180)/1.5 # roll rate [rad/s]
 da = 25 *(pi/180) #rad/s
-b1 =  0.7*b/2 #m
+b1 =  0.7*b/2 #first limit [m]
 tau = 0.42
 
 #Values still to be determined by other groups
 c_l_alpha = 0.0999 *180/pi #1/rad #Based on tip airfoil
-c_d0 = 0.007     #Based on 
+c_d0 = 0.007     #zero lift drag coefficient
 ca_c = 0.2 #Aileron chord to wing chord ratio
 
 #Integration parameters
